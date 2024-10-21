3a58ec959499ccb1dee372a701157053c341a1c6
gugagogitidze <gugsonsss31@gmail.com>
2024-10-21 17:24:27 +0400

    egaru
    nnnnn

---
+++ script.js
@@ -0,0 +1,40 @@
+//1)
+
+let num = 25;
+let square = num * num;
+
+if (square > 100) {
+  console.log("მეტია ასზე");
+} else {
+  console.log("ნაკლებია ასზე");
+}
+
+//2)
+
+let number = 75;
+if (number > 50 && number < 100) {
+  console.log("რიცხვი 50-ზე მეტია და 100ზე ნაკლები");
+} else {
+  console.log("რიცხვი არ არის 50ზე მეტი ან 100ზე ნაკლები");
+}
+
+//3)
+
+let ricxvi = -35;
+if (number <= 0) {
+  console.log("რიცხვი ნულის ტოლია ან უარყოფითი");
+} else {
+  console.log("რიცხვი დადებითია");
+}
+
+//4)
+
+let lenght = 60;
+let width = 44;
+let perimeter = 2 * (lenght + width);
+
+if (perimeter > 50) {
+  console.log("პერიმეტრი მეტია 50ზე");
+} else {
+  console.log("პერიმეტრი ნაკლებია 50-ზე");
+}
